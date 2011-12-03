8b0fee73355c335f1babef1d009f92ce1e4b1f6b
user <user@user01-PC>
2011-12-03 15:00:52 +0900

    ref

--- vCloudForJava5/src/my/VMDetails.java
+++ vCloudForJava5/src/my/VMDetails.java
@@ -1,14 +1,3 @@
-/*
- * *******************************************************
- * Copyright VMware, Inc. 2010-2011.  All Rights Reserved.
- * *******************************************************
- *
- * DISCLAIMER. THIS PROGRAM IS PROVIDED TO YOU "AS IS" WITHOUT
- * WARRANTIES OR CONDITIONS # OF ANY KIND, WHETHER ORAL OR WRITTEN,
- * EXPRESS OR IMPLIED. THE AUTHOR SPECIFICALLY # DISCLAIMS ANY IMPLIED
- * WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY # QUALITY,
- * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
- */
 package my;
 
 import java.io.IOException;
@@ -35,8 +24,8 @@ import com.vmware.vcloud.sdk.admin.User;
 import com.vmware.vcloud.sdk.constants.Version;
 import com.vmware.vcloud.sdk.samples.extra.FakeSSLSocketFactory;
 
-/*
- * This sample lists all the vdc's (Name, Allocation Model), vapp's (Name) and its vms (Name, Status, Cpu, Memory & HardDisks).
+/**
+ *
  *
  */
 
@@ -73,76 +62,81 @@ public class VMDetails {
 				for (ReferenceType vAppRef : Vdc.getVdcByReference(
 						vcloudClient, vdcRef).getVappRefs()) {
 
-					System.out.println("	Vapp : " + vAppRef.getName());
-
-					// ------------
-					System.out.println("	Vapp_OtherAttributes : "
-							+ vAppRef.getOtherAttributes().size());
-					System.out.println("	Vapp_VCloudExtension : "
-							+ vAppRef.getVCloudExtension().size());
-					// ------------
-
-					// ------------
-					ReferenceType owner = Vapp.getOwner(vcloudClient, vAppRef);
-					System.out.println(owner);
-
-					try {
-						User user = User
-								.getUserByReference(vcloudClient, owner);
-
-						UserType resource = user.getResource();
-
-						System.out.println("★" + resource.getEmailAddress());
-						System.out.println("★" + resource.getFullName());
-					} catch (Exception e) {
-						// TODO 自動生成された catch ブロック
-						e.printStackTrace();
-					}
-
-					/**
-					 * AccessSettingsType settings = controlAccess
-					 * .getAccessSettings();
-					 *
-					 * if (settings != null) {
-					 *
-					 * for (AccessSettingType set : settings
-					 * .getAccessSetting()) { AccessLevelType accessLevel =
-					 * set.getAccessLevel();
-					 *
-					 *
-					 * System.out.println("	Vapp_AccessLevel : " +
-					 * accessLevel.name() + "\t" + set.getSubject().getHref());
-					 * User user = User.getUserByReference(vcloudClient,
-					 * set.getSubject());
-					 * System.out.println("	Vapp_UserInfoMail : " +
-					 * user.getResource().getEmailAddress());
-					 * System.out.println("	Vapp_UserInfo : " +
-					 * user.getResource().getName()); } }else{
-					 * System.out.println("	Vapp_ACC_INFO_NULL : "); }
-					 */
-
-					Vapp vapp = Vapp.getVappByReference(vcloudClient, vAppRef);
-					List<VM> vms = vapp.getChildrenVms();
-					for (VM vm : vms) {
-						System.out.println("		Vm : "
-								+ vm.getResource().getName());
-						System.out.println("			Status : " + vm.getVMStatus());
-						System.out.println("			CPU : "
-								+ vm.getCpu().getNoOfCpus());
-						System.out.println("			Memory : "
-								+ vm.getMemory().getMemorySize() + " Mb");
-						for (VirtualDisk disk : vm.getDisks()) {
-							if (disk.isHardDisk())
-								System.out.println("			HardDisk : "
-										+ disk.getHardDiskSize() + " Mb");
-						}
-
-					}
+					showVApp(vcloudClient, vAppRef);
 
 				}
 
 			}
 		}
 
+	}
+
+	private static void showVApp(VcloudClient vcloudClient,
+			ReferenceType vAppRef) throws VCloudException {
+		System.out.println("	Vapp : " + vAppRef.getName());
+
+		// ------------
+		System.out.println("	Vapp_OtherAttributes : "
+				+ vAppRef.getOtherAttributes().size());
+		System.out.println("	Vapp_VCloudExtension : "
+				+ vAppRef.getVCloudExtension().size());
+		// ------------
+
+		// ------------
+		ReferenceType owner = Vapp.getOwner(vcloudClient, vAppRef);
+		System.out.println(owner);
+
+		try {
+			User user = User
+					.getUserByReference(vcloudClient, owner);
+
+			UserType resource = user.getResource();
+
+			System.out.println("★" + resource.getEmailAddress());
+			System.out.println("★" + resource.getFullName());
+		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		/**
+		 * AccessSettingsType settings = controlAccess
+		 * .getAccessSettings();
+		 *
+		 * if (settings != null) {
+		 *
+		 * for (AccessSettingType set : settings
+		 * .getAccessSetting()) { AccessLevelType accessLevel =
+		 * set.getAccessLevel();
+		 *
+		 *
+		 * System.out.println("	Vapp_AccessLevel : " +
+		 * accessLevel.name() + "\t" + set.getSubject().getHref());
+		 * User user = User.getUserByReference(vcloudClient,
+		 * set.getSubject());
+		 * System.out.println("	Vapp_UserInfoMail : " +
+		 * user.getResource().getEmailAddress());
+		 * System.out.println("	Vapp_UserInfo : " +
+		 * user.getResource().getName()); } }else{
+		 * System.out.println("	Vapp_ACC_INFO_NULL : "); }
+		 */
+
+		Vapp vapp = Vapp.getVappByReference(vcloudClient, vAppRef);
+		List<VM> vms = vapp.getChildrenVms();
+		for (VM vm : vms) {
+			System.out.println("		Vm : "
+					+ vm.getResource().getName());
+			System.out.println("			Status : " + vm.getVMStatus());
+			System.out.println("			CPU : "
+					+ vm.getCpu().getNoOfCpus());
+			System.out.println("			Memory : "
+					+ vm.getMemory().getMemorySize() + " Mb");
+			for (VirtualDisk disk : vm.getDisks()) {
+				if (disk.isHardDisk())
+					System.out.println("			HardDisk : "
+							+ disk.getHardDiskSize() + " Mb");
+			}
+
+		}
 	}
 }
